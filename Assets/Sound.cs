4cac8b391343bb2a2fb1f8931cb4f23baffa8221
Josef Jankowski <jankowski.josef99@gmail.com>
2021-02-23 16:26:07 -0800

    Add audio scripts.

---
+++ Assets/Sound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound 
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0f, 1f)]
+    public float pitch;
+
+    // TODO(josefj1519):Make private and use a getter
+    [HideInspector]
+    public AudioSource source; 
+}
