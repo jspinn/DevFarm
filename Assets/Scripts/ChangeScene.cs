34396ecddfaf3a98d6caa521173e6b2acaa3bb55
Sydnie Nguyen <sydnienguyen@csu.fullerton.edu>
2021-03-12 23:12:47 -0800

    Created Step 1,back and next buttons. How To Play Button goes to Step 1

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+   
+    // This will change the scene when a button is clicked
+    // Ex. Clicking How To Play will go into a tutorial of how to play the game
+    public void ChangeToScene(string SceneToChangeTo)
+    {
+        Application.LoadLevel(SceneToChangeTo);
+    }
+}
