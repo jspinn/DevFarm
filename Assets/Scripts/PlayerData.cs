837469a89d4c953e091a06d0dfc63954f8880d8b
Jay <jaydenspinn@gmail.com>
2021-03-16 21:15:57 -0700

    Started template for save system

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public int money;
+
+    public PlayerData ()
+    {
+        // Get data from player/money system
+    }
+
+}
