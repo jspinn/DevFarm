28221fbdd700b527001b41cf9135f2e62c6f7e05
unknown <jaydenspinn@gmail.com>
2021-03-24 21:15:58 -0700

    Desk building

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake() {
+        if (instance != null) {
+            Debug.LogError("More than one BuildManager in scene.");
+        }
+
+        instance = this;
+    }
+
+
+    [SerializeField] GameObject standardDeskPrefab;
+
+    private GameObject deskToBuild;
+
+    void Start() {
+        deskToBuild = standardDeskPrefab;
+    }
+
+    public GameObject GetDeskToBuild() {
+        return deskToBuild;
+    }
+
+    public void BuildDesk(DeskNode node) {
+        GameObject desk = (GameObject)Instantiate(deskToBuild, node.GetBuildPosition(), transform.rotation);
+        node.desk = desk;
+    }
+
+    
+}
---
+++ Assets/Scripts/DeskNode.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class DeskNode : MonoBehaviour
+{
+
+    [SerializeField] private Color hoverColor;
+    [SerializeField] private Color clickColor;
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private GameObject buildMenu;
+
+    // which desk is on this node
+    public GameObject desk;
+
+    private SpriteRenderer render;
+    private Color defaultColor;
+    private BuildManager buildManager;
+
+    void Start() {
+        render = GetComponent<SpriteRenderer>();
+        defaultColor = render.color;
+        buildManager = BuildManager.instance;
+        
+    }
+    
+
+   void OnMouseEnter() {
+        Debug.Log("Mouse Enter");
+        render.color = hoverColor;
+   }
+
+   void OnMouseExit() {
+        Debug.Log("Mouse Exit");
+        render.color = defaultColor;
+   }
+
+   void OnMouseDown() {
+        render.color = clickColor;
+
+        if (desk != null) {
+            // upgrade
+        }
+        else {
+            buildManager.BuildDesk(this);
+        }
+   }
+
+   void OnMouseUp() {
+       render.color = hoverColor;
+   }
+
+   public Vector3 GetBuildPosition() {
+       return transform.position + positionOffset;
+   }
+}
