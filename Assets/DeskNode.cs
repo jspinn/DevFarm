7378c0cc142a8314236d54ef0615a68fec3c1cb7
Jay <jaydenspinn@gmail.com>
2021-03-24 19:15:47 -0700

    Added desk nodes

---
+++ Assets/DeskNode.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DeskNode : MonoBehaviour
+{
+   void OnMouseEnter() {
+       Debug.Log("Mouse Enter");
+   }
+
+   void OnMouseExit() {
+       Debug.Log("Mouse Exit");
+   }
+
+   void OnMouseDown() {
+       Debug.Log("Mouse Click");
+   }
+}
