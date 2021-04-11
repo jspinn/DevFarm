3c7d7970adddf577701f7dcbeb7c2cd9b6bfd08c
Sydnie Nguyen <sydnienguyen@csu.fullerton.edu>
2021-04-10 17:41:37 -0700

    changed play to new game and load game button

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class ChangeScene : MonoBehaviour
 {
-   
     // This will change the scene when a button is clicked
     // Ex. Clicking How To Play will go into a tutorial of how to play the game
     public void ChangeToScene(string SceneToChangeTo)
