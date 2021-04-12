c012d3e4e8590112fe475f4650179588cb8cac07
Sydnie Nguyen <sydnienguyen@csu.fullerton.edu>
2021-04-11 23:41:43 -0700

    got money to display on UI

--- Assets/moneySystem/PlayerMoney.cs
+++ Assets/moneySystem/PlayerMoney.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.UI;
+using UnityEngine.UI;
+using TMPro;
 
 public class playerMoney : MonoBehaviour
 {
     public int money;
-    //public Text moneyText;
+    public TextMeshProUGUI moneyText;
     // Start is called before the first frame update
     void Start()
     {
         money = 10000;
+        moneyText.text= money.ToString();
     }
 
     // Update is called once per frame
