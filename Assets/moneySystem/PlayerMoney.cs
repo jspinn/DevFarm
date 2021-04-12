6d3109e6f91c3be410541c7ee7496419eb5b63f4
Sydnie Nguyen <sydnienguyen@csu.fullerton.edu>
2021-04-11 23:26:57 -0700

    renamed Player Money file to playerMoney to be compatible to Unity

---
+++ Assets/moneySystem/PlayerMoney.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class playerMoney : MonoBehaviour
+{
+    public int money;
+    //public Text moneyText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        money = 10000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addMoney(int moneyToAdd)
+    {
+        money += moneyToAdd;
+
+    }
+
+    public void subtractMoney(int moneyToSubtract)
+    {
+        if(money - moneyToSubtract<0)
+        {
+            Debug.Log("Not enough money!");
+        }
+        else
+        {
+            money -= moneyToSubtract;
+        }
+
+    }
+}
